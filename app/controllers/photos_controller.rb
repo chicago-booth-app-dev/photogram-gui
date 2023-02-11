5fa9dbe52dd6aa3f72e1ea0e9bf8676db1555f77
Will Moeller <willmoeller@gmail.com>
2023-02-11 20:26:31 +0000

    Add delete action for photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
   def index
-    @photos = Photo.all
+    @photos = Photo.all.order({ :created_at => :desc })
     render({ :template => "photo_templates/index.html.erb" })
   end
 
@@ -8,5 +8,12 @@ class PhotosController < ApplicationController
     photo_id = params.fetch("photo_id")
     @photo = Photo.all.where({ :id => photo_id }).first
     render({ :template => "photo_templates/photo.html.erb" })
+  end
+
+  def delete
+    photo_id = params.fetch("photo_id")
+    @photo = Photo.all.where({ :id => photo_id }).first
+    @photo.destroy
+    redirect_to("/photos") # send them to a page after the photo is deleted
   end
 end
